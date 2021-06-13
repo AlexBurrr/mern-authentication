689cad96038de900cc724183588a5926d283b103
Alex Berumen <alexandroberumen@gmail.com>
2021-06-13 12:50:49 -0700

    connected server file to mongo db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+
+dotenv.config();
+
+
+
+////////////////SET UP SERVER/////////////////
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 })
@@ -13,6 +19,13 @@ app.get('/', (req, res) => {
     res.send('home')
 })
 
-app.get("/test", (req, res) => {
-    res.send('test working')
-}) 
+
+//CONNECT TO MONGODB//////////////////////////
+
+mongoose.connect(process.env.MDB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err => {
+    if (err) return console.log(err);
+    console.log('connected to mongodb');
+}))
