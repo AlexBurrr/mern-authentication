1fd8679e12b094a7639f9b8e90351b32bcc70ba1
Alex Berumen <alexandroberumen@gmail.com>
2021-06-13 15:34:25 -0700

    hashed the password and created cookie from jwt

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/userModel')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 
@@ -8,7 +9,7 @@ router.post('/', async (req, res) => {
     try {
         const { email, password, passwordConfirm } = req.body;
 
-        //validation
+        //VALIDATION
         if (!email || !password || !passwordConfirm)
             return res
                 .status(400)
@@ -29,6 +30,37 @@ router.post('/', async (req, res) => {
                 .status(400)
                 .json({ errorMessage: 'an account with this email already exists' });
         }
+
+        //HASH THE PASSWORD
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+
+        //////////        //SAVE A NEW USER ACCOUNT TO THE DB////////////////////////////////////////////
+
+        const newUser = new User({
+            email, passwordHash
+        });
+
+        const savedUser = await newUser.save()
+
+        //SIGN THE TOKEN////////////////////////////////////////
+
+        const token = jwt.sign({
+            user: savedUser._id
+        }, process.env.JWT_SECRET)
+
+
+        console.log(`token ${token}`);
+
+
+        //SEND THE TOKEN IN A HTTP-ONLY COOKIE
+
+        res.cookie('token', token, {
+            httpOnly: true,
+
+        }).send()
+
 
     } catch (error) {
         console.error(error);
